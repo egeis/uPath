b90c22a05ec446d909c8e0b6a2103be7148e6db9
Egeis <no-reply@blocklands.net>
2014-07-02 11:19:59 -0700

    Added Adjacent ref storage for Nodes

--- Assets/Scripts/Node.cs
+++ Assets/Scripts/Node.cs
@@ -7,6 +7,17 @@ public class Node : MonoBehaviour {
 	public bool obstructed { get; set; }
 	public int X { get; set; }
 	public int Y { get; set; }
+		
+	private Node[] _adjacent = new Node[8];
 	
-	//private Node[] _adjacent = new Node[8];
+	public bool addAdj(ref Node n, int index) {
+		if(index < 8 && index > -1 && n != null) {
+			_adjacent[index] = n;
+			return true;
+		}
+		
+		return false;
+	}
+	
+	public Node getAdj() { return _adjacent; }
 }
