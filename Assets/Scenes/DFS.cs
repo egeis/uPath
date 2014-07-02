5c5db408c4e6107bdb7c855458e9e8c8ecaecc1e
Egeis <no-reply@blocklands.net>
2014-07-01 19:01:50 -0700

    Initial Commit

---
+++ Assets/Scenes/DFS.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DFS : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
---
+++ Assets/Scenes/Node.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using System.Collections;
+
+/**
+ * Generic Node Class
+ */
+public class Node : MonoBehaviour {
+	public Node parent;
+	public Node[] children;
+	public bool used = false;
+}
