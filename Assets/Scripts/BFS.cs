976cf33227ecf36f5e11ed0a0ce317746d72c716
egeis <rcoan@callutheran.edu>
2014-07-05 12:41:43 -0700

    Initial BFS methods and returns

--- Assets/Scripts/BFS.cs
+++ Assets/Scripts/BFS.cs
@@ -1,6 +1,14 @@
 ﻿using UnityEngine;
 using System.Collections;
+using System.Collections.Generic;
 
 public class BFS : MonoBehaviour {
-
+	private List<Node> _path = new List<Node>();
+	
+	public bool Search(Node _start) {
+	
+		return false;
+	}
+	
+	public List<Node> GetPath() { return _path; }
 }
